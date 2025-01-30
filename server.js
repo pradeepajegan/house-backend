91453681b15b1cee83e83902f7184ee6485de84c
pradeepamurugan <153383822+pradeepajegan@users.noreply.github.com>
2025-01-30 20:17:24 +0530

    Updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "house-frontend-blh6lyuzr-pradeepas-projects-f43c1da3.vercel.app" }));
+app.use(cors());
 const MONGO_URI = "mongodb+srv://pradeepa-1204:pradeepa1204@cluster0.c1hoy.mongodb.net/houseRentDB?retryWrites=true&w=majority";
 const JWT_SECRET = "secret_key";
 const PORT = process.env.PORT || 5000;
